2fc7be60a927b0e573726f1ff3a7d0933d10baab
Ramsey lewis <ramseylewis99@gmail.com>
2020-11-05 12:17:15 -0500

    Cards Working

--- scripts.js
+++ scripts.js
@@ -8,35 +8,45 @@ let content = document.getElementById('content');
 function search(){
     // Created So That if search was just done you cant do it again
     if( query !== document.getElementById('search').value){
-    query = document.getElementById('search').value;
-   
-    url = `http://www.omdbapi.com/?s=${query}&apikey=88bd5903`;
+      query = document.getElementById('search').value;
+    
+      url = `http://www.omdbapi.com/?s=${query}&apikey=88bd5903`;
 
-    //ASYNCHRONUS CODE
-    fetch(url)
-        .then (res => res.json())
-        .then(res => web(res.Search))
-        .catch(err => console.log(err)) 
+      //ASYNCHRONUS CODE
+      fetch(url)
+          .then (res => res.json())
+          .then(res => web(res.Search))
+          .catch(err => console.log(err)) 
     }
 }
 // res == all of my API infromation 
 function web(res){
-
+  console.log(res)
   for(let i = 0; i < res.length; i++){
-     console.log(res)
+    
 
   
     let picture = document.createElement("img")
-    let card = document.createElement('div') 
+    let flipCard = document.createElement('div') 
+    let flipCardInner = document.createElement('div') 
+    let flipCardFront = document.createElement('div') 
+    let flipCardBack = document.createElement('div') 
     let title = document.createElement('div')
 
   
-    card.setAttribute('class', 'card')  
+    flipCard.setAttribute('class', 'flip-card')  
+    flipCardInner.setAttribute('class', 'flip-card-inner')  
+    flipCardFront.setAttribute('class', 'flip-card-front')  
+    flipCardBack.setAttribute('class', 'flip-card-back')  
+
+
+
+
     picture.setAttribute('class', 'posterImg')
     title.setAttribute('class', 'title')
   
     picture.src = res[i].Poster
-    title.src = res[i].Title
+    title.innerText = res[i].Title
     console.log(title.src)
 
     
@@ -44,9 +54,15 @@ function web(res){
     if(picture.src.slice(-3) == "N/A"){
       picture.src ='./assets/noImage.jpg'
     }
-    card.appendChild(title)
-    card.appendChild(picture)
-    content.appendChild(card) 
+
+    // Read from bottom is HTML indentation 
+          flipCardBack.appendChild(title)
+        flipCardInner.appendChild(flipCardBack) 
+          flipCardFront.appendChild(picture)
+        flipCardInner.appendChild(flipCardFront)
+      flipCard.appendChild(flipCardInner)
+
+    content.appendChild(flipCard) 
   }
 }
 
