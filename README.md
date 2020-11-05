df1e02a29882f1b67772592aa9d822dd580f42db
Ramsey lewis <ramseylewis99@gmail.com>
2020-11-05 01:05:01 -0500

    working on adding dropdown to card

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ TO DO LIST
 [] Set Information in inofrmation searh to hover
 []MAKE IT SO THAT IF YOU HOVER OVER CARD WITH MOUSE MOVIE GREYS OUT under the text
 []ADD DETAILS LIKE PRICE OF NETFLIX AT BOTTOM OS SCREEN
+[] make search depoplaute when new search is made
 
 /////////REACH
 
--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 
 let query = ''; 
 let url = `http://www.omdbapi.com/?t=${query}&apikey=88bd5903`;
+let infoUrl = `http://www.omdbapi.com/?i=${query}&apikey=88bd5903`;
 let content = document.getElementById('content');
 
-// http://www.omdbapi.com/?i=tt0234215&apikey=88bd5903
 
 function search(){
     // Created So That if search was just done you cant do it again
@@ -19,28 +19,32 @@ function search(){
         .catch(err => console.log(err)) 
     }
 }
-
 // res == all of my API infromation 
 function web(res){
-  //RUNNING FOR LOOP IN FUCNTION BREAKS CODE .... WHY??? ////////
+
   for(let i = 0; i < res.length; i++){
-    console.log(res)
+     console.log(res)
 
   
     let picture = document.createElement("img")
     let card = document.createElement('div') 
+    let title = document.createElement('div')
 
   
-    card.setAttribute('class', 'card')
+    card.setAttribute('class', 'card')  
     picture.setAttribute('class', 'posterImg')
+    title.setAttribute('class', 'title')
   
     picture.src = res[i].Poster
+    title.src = res[i].Title
+    console.log(title.src)
+
     
-    console.log(picture.src.slice(-3))
+    // if the search comes up wihtout a picture add the noImage jpg located in assets
     if(picture.src.slice(-3) == "N/A"){
       picture.src ='./assets/noImage.jpg'
     }
-
+    card.appendChild(title)
     card.appendChild(picture)
     content.appendChild(card) 
   }
@@ -54,5 +58,6 @@ window.onkeydown = (event) => {
   //   alert('Your search did not have any results.')
   }
 }
+
 // alterSearch()
 // onLoad()
